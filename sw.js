35ca938eb7d643522f6bf7bd35d7189559547c38
thatkidtk <thomaskase14@gmail.com>
2025-08-31 07:15:31 -0400

    feat: TaskFlow PWA with activity feed, filters, and task editing

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'taskflow-cache-v1';
+const ASSETS = [
+  './team-task-manager.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
+      return resp;
+    }).catch(() => cached))
+  );
+});
+
